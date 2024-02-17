137ac1c3bea76e622149b4da97010034645b8b49
thycao <thycao09@gmail.com>
2024-02-17 04:31:50 -0600

    updated

--- src/thy/com/homework/JavaBasic/JavaBasic4/VongLapForandArray.java
+++ src/thy/com/homework/JavaBasic/JavaBasic4/VongLapForandArray.java
@@ -3,23 +3,19 @@ package thy.com.homework.JavaBasic.JavaBasic4;
 public class VongLapForandArray {
     public static void main(String[] args) {
         // Cau 1, tao vong lap For in ra so chan tu 0-50
-        System.out.println("--------------Cau 1, tao vong lap For in ra so chan tu 0-50----------------");
-        int sochan[] = new int[51];
+        System.out.println("-----------Cau 1, tao vong lap For in ra so chan tu 0-50------------");
         int mangluusochan[] = new int[51];
         for (int i = 0; i <= 50; i++) {
             if (i % 2 == 0) {
-                sochan[i] = i;
-                System.out.println(sochan[i]);
-                // Cau 2, Tao mang de luu nhung so chan do vao
-                mangluusochan[i] = sochan[i];
+                mangluusochan[i] = i; //tao mang de gan nhung so chan nay vao
+                System.out.println(i);
             }
-
         }
-        System.out.println("--------------In ra mang luu so chan vua tao de duyet xem ket qua----------------");
+        System.out.println("------------Duyet mang in ra ket qua sau khi gan--------------");
         for (int i = 1; i < mangluusochan.length; i++) {
             System.out.println(mangluusochan[i]);
         }
-        System.out.println("---------------END---------------");
+        System.out.println("------------------------END-----------------------------------");
     }
 }
 
